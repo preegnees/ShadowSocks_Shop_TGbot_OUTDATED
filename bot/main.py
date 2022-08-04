8d97dea69a89f03991b54fd296b7cd25c0b1f4ad
preegnees <mrraziapov13@gmail.com>
2022-08-04 17:59:10 +0300

    добавил логгирование и  подгрузку переменных окружения

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,14 @@
+from loguru import logger
+from dotenv import dotenv_values
+
+def load_conf() -> dict:
+    config = dotenv_values("config.env")
+    return config
+
 def main():
-    print("run")
+    load_conf()
+    logger.info("envs seccesful loaded")
 
 if __name__ == '__main__':
+    logger.info("start app")
     main()
