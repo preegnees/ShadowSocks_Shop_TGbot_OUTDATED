6514ef767485948841750537b1ff062e0d1c65b1
preegnees <mrraziapov13@gmail.com>
2022-08-05 16:09:16 +0300

    в main теперь подгружаю envs, инициализирую Storage и внедряю его к обработчикам, запускаю бот

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,20 @@
 from loguru import logger
 from dotenv import dotenv_values
+from ui.initialize import run_bot
+from database.storage import Storage
+from ui.handler import set_storage_to_handlers
 
 def load_conf() -> dict:
     config = dotenv_values("config.env")
     return config
 
 def main():
-    load_conf()
+    config = load_conf()
     logger.info("envs seccesful loaded")
+    storage = Storage(config)
+    set_storage_to_handlers(storage)
+    logger.info("database is initialized")
+    run_bot(config)
 
 if __name__ == '__main__':
     logger.info("start app")
