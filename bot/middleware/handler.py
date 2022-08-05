d2d9dea08296d7925c78ad7020c0777b324124a0
preegnees <mrraziapov13@gmail.com>
2022-08-05 19:39:33 +0300

    изменил хендлер, неважно

---
+++ bot/middleware/handler.py
@@ -0,0 +1,15 @@
+from loguru import logger
+
+storage = None
+
+def inject_storage(strg):
+    global storage
+    storage = strg
+    logger.debug("Произошло инжек хранилища в файл handler")
+    
+
+def process_start_command_handler(tg_id: str, fullname: str) -> str:
+    storage.set_user(fullname, tg_id)
+    (username, id) = storage.get_user(tg_id)
+    return f"username: {username}, id: {id}"
+    
