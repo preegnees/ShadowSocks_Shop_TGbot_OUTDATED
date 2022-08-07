98daca2dc998817bd28848c5f8b9d1ecea91cf6c
preegnees <mrraziapov13@gmail.com>
2022-08-07 21:47:46 +0300

    написал тест для функции получения истории и мок для нее

--- bot/middleware/payment/test_pay.py
+++ bot/middleware/payment/test_pay.py
@@ -1,4 +1,4 @@
-from payment.pay import get_status
+from payment.pay import get_status, get_history
 import pytest
 import time
 
@@ -13,6 +13,14 @@ class Storage_Mock2():
         return ["test_user","test_id_3456"] 
     def get_last_transaction(self, id: str):
         return ["test_id_3456", "label_test", int(time.time() * 1000), 500, 0]
+    
+class Storage_Mock3():
+    def get_user(self, id: str):
+        return ["test_user","test_id_3456"] 
+    def get_all_transaction(self, id: str):
+        return [{"id_tg": "test_id_3456", "lable": "label_test", "time_created": int(time.time() * 1000) + 2, "amount": 356, "confirmed": 0}, 
+                {"id_tg": "test_id_3456", "lable": "lolkekcheburel", "time_created": int(time.time() * 1000) + 1, "amount": 778, "confirmed": 1},
+                {"id_tg": "test_id_3456", "lable": "pochemutakblyat", "time_created": int(time.time() * 1000) + 0, "amount": 34, "confirmed": 1}]
 
 def test_get_status():
     storage = Storage_Mock()
@@ -20,3 +28,8 @@ def test_get_status():
     storage2 = Storage_Mock2()
     status2 = get_status(storage2, "test_id_3456")
     assert(status != status2)
+
+def test_get_history():
+    storage = Storage_Mock3()
+    history = get_history(storage, "test_id_3456")
+    assert("-->1" in history and "-->2" in history)
