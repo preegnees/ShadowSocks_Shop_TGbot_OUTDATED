661486d65a20a2b2e5425a9478cbfb8058e32ce5
preegnees <mrraziapov13@gmail.com>
2022-08-07 20:38:15 +0300

    Добавил в интерфейс методы по сохранение транзакции, подтверждения, получение последней и получение всх. Исправил названия методов

--- bot/database/database/storage.py
+++ bot/database/database/storage.py
@@ -1,13 +1,26 @@
 from loguru import logger
-from .api_mongo import init, save_label, save_user, get_user
+from .api_mongo import init_m, save_user_m, get_user_m, save_transaction_m, get_all_transaction_m, get_last_transaction_m, confirm_transaction_m
 
 class IStorage:
-    def set_lable(self, lable: str, id_tg: str):
+    def get_user(self, id_tg: str):
         pass
     
-    def set_user(self, username: str, id_tg: str):
+    def save_user(self, username: str, id_tg: str):
         pass
-
+    
+    
+    
+    def save_transaction(seld, lable: str, id_tg: str, time_created: int, amount: int, confirmed: bool):
+        pass
+    
+    def get_all_ltransactions(id_tg: str):
+        pass
+    
+    def get_last_transaction(id_tg: str):
+        pass
+    
+    def confirm_transaction(lable: str):
+        pass
 
 class Storage(IStorage):
     config = None
@@ -15,15 +28,25 @@ class Storage(IStorage):
 
     def __init__(self, cnf):
         self.config = cnf
-        self.clinet = init(self.config)
-        
-
-    def set_lable(self, label: str, id_tg: str):
-        save_label(label, id_tg)
+        self.clinet = init_m(self.config)
     
-    def set_user(self, username: str, id_tg: str):
-        save_user(username, id_tg)
+    def save_user(self, username: str, id_tg: str):
+        save_user_m(username, id_tg)
         
     def get_user(self, id_tg: str):
-        (username, tg_id) = get_user(id_tg)
+        (username, tg_id) = get_user_m(id_tg)
         return username, tg_id
+    
+    
+    
+    def save_transaction(seld, lable: str, id_tg: str, time_created: int, amount: int):
+        save_transaction_m(lable, id_tg, time_created, amount)
+    
+    def get_all_ltransactions(id_tg: str):
+        pass
+    
+    def get_last_transaction(id_tg: str):
+        get_last_transaction_m(id_tg)
+    
+    def confirm_transaction(lable: str):
+        confirm_transaction_m(lable)
