b96a892078c9fb236a508a75c78c9573466a75e4
preegnees <mrraziapov13@gmail.com>
2022-08-07 20:35:46 +0300

    исправил название функции

--- bot/middleware/handler.py
+++ bot/middleware/handler.py
@@ -9,7 +9,7 @@ def inject_storage(strg):
     
 
 def process_start_command_handler(tg_id: str, fullname: str) -> str:
-    storage.set_user(fullname, tg_id)
+    storage.save_user(fullname, tg_id)
     (username, id) = storage.get_user(tg_id)
     return f"username: {username}, id: {id}"
     
